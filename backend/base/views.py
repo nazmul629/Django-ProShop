a550c6deab3d3bed416795b6c245fbb9602116ad
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-04-06 23:36:52 +0600

    Data lode from backend databse

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,22 +3,25 @@ from django.http import JsonResponse
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product 
+from .serializers import ProductSerializer
+
 from .products import products
     
 @api_view(['GET'])
 def getRoutes(request):
+
     return Response('Hello World')
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializers = ProductSerializer(products, many = True)
+
+    return Response(serializers.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for  i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-         
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializers = ProductSerializer(product, many = False)
+    return Response(serializers.data)
